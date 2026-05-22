7e415a8240220fae5995d4149e804c9d3a647c11
Ezequiel <ezequiel.sanabria1@gmail.com>
2026-05-22 12:17:18 -0300

    ultimos detalles

--- src/data/data.ts
+++ src/data/data.ts
@@ -10,9 +10,9 @@ export interface CartItem extends Product {
 }
 
 export const PRODUCTS: Product[] = [
-    { id: 1, nombre: 'Hamburguesa Doble', precio: 8500, descripcion: 'Pan con sesamo, medallón de carne con lechuga y tomate.', imagen: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400', categoriaId: 1 },
-    { id: 2, nombre: 'Pizza Especial', precio: 7000, descripcion: 'Muzarella y jamón.', imagen: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?w=400', categoriaId: 2 },
-    { id: 3, nombre: 'Papas Fritas con cheddar', precio: 5000, descripcion: 'Papas saladas.', imagen: 'https://images.unsplash.com/photo-1573080496219-bb080dd4f877?w=400', categoriaId: 3 }
+    { id: 1, nombre: 'Hamburguesa Doble', precio: 8500, descripcion: 'Pan con sesamo, medallón de carne con lechuga, tomate, queso y cebolla.', imagen: 'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', categoriaId: 1 },
+    { id: 2, nombre: 'Pizza Napolitana con jamón', precio: 7000, descripcion: 'Muzarella y jamón con tomate.', imagen: 'https://plus.unsplash.com/premium_photo-1673439304183-8840bd0dc1bf?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', categoriaId: 2 },
+    { id: 3, nombre: 'Papas Fritas con perejil', precio: 5000, descripcion: 'Papas saladas con sabor exquisito.', imagen: 'https://images.unsplash.com/photo-1630431341973-02e1b662ec35?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', categoriaId: 3 }
 ];
 
 export const CATEGORIES: Icategoria[] = [
